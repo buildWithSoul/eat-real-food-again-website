77be44c52d716a6d96bb0d4267108ac8f4244d8d
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-11-22 22:34:45 -0800

    create and style Healla component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Hero from "@/components/Hero/Hero";
 import JoinMovement from "@/components/JoinMovement/JoinMovement";
 import Goals from "@/components/Goals/Goals";
+import Healla from "@/components/Healla/Healla";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <MissionStatement />
         <JoinMovement />
         <Goals />
+        <Healla />
       </main>
       <footer className={styles.footer}></footer>
     </div>
