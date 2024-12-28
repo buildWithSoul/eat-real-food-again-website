0195642b9f55bafd1ead371d036481155d307a51
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-12-28 15:35:29 -0800

    add auth card image

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -37,7 +37,7 @@ const Auth = () => {
     <Box className={styles.card}>
       <Image
         className={styles.cardImage}
-        src="https://s3-alpha-sig.figma.com/img/d5d8/0667/b922df06ea05d359b8fb267006e47a0b?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ne4bSsiXcikavj3eSw-wjOxaf0--VzNAiJ7TiXDT20IcxS5RGtC24g7tdnDs3YI3O7wCbJWVISyT9Wln~mjxIBXe-VEUxp2mZEPM3OwFN2lKnu-kESzwoK1giGNGmKAkRWaidW4o70wWQFDuAVtF-kwQEZz87NNFEXDrm2PmMixlT1h~wEsEjFL7BdmRUn7uLEWfRI1S~Yz7hL9mldKNuBdF-2WHjOt1B-VGkJ-6umUrT16yOrd~TDd7G03tLAsVfIcIBX7WEh2ugHB~JZxJV6lqnLHqDLnQ4YD1QzbLnI8UyMWuseA8c7ED1qf9mTLnnaSh8vSXRFHILnzFU55fOw__"
+        src="/image-auth.png"
         alt="Join The Movement"
       />
 
