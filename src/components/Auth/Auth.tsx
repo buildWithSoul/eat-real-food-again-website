db445aba28ef1ade932142a9b737725cc93f5bf5
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-12-28 17:26:57 -0800

    removed unused imports

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,32 +1,24 @@
 "use client";
 
-import { Box, Button, Divider, Image, Text, Title } from "@mantine/core";
-import Link from "next/link";
-import styles from "../Auth/Auth.module.css";
-import { supabase } from "../../utils/supabase/client";
 import SignUp from "./SignUp";
 import { useState } from "react";
 
-type AUTH_VIEW = 'signup' | 'login' | 'forgot_password'
-
+type AUTH_VIEW = "signup" | "login" | "forgot_password";
 
 const Auth = () => {
-  const [selectedView, setSelectedView] = useState<AUTH_VIEW>('signup')
+  const [selectedView, setSelectedView] = useState<AUTH_VIEW>("signup");
 
   const chosenView = () => {
-    if (selectedView === "signup") return <SignUp />
+    if (selectedView === "signup") return <SignUp />;
 
-    if (selectedView === 'login') return <SignUp />
+    if (selectedView === "login") return <SignUp />;
 
-    if (selectedView === 'forgot_password') return <SignUp />
+    if (selectedView === "forgot_password") return <SignUp />;
 
-    return <SignUp />
-  }
+    return <SignUp />;
+  };
 
-  return (<>
-      {chosenView()}
-  </>
-  );
+  return <>{chosenView()}</>;
 };
 
 export default Auth;
